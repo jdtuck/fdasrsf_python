fbfd51ef30673dd7ea75b8bb6e253b1f5987cf77
Derek Tucker <jdtuck@sandia.gov>
2017-01-11 13:55:26 -0700

    Updated DP

--- src/DP.c
+++ src/DP.c
@@ -30,52 +30,58 @@ const int Nbrs[NNBRS][2] = {
 };
 
 int xycompare(const void *x1, const void *x2);
-double CostFn2(const double *q1, const double *q2, const double *q2L, int k, int l, int i, int j, int n, int N, int M, double lam);
+double CostFn2(const double *q1L, const double *q2L, int k, int l, int i, int j, int n, int scl, double lam);
 void thomas(double *x, const double *a, const double *b, double *c, int n);
 void spline(double *D, const double *y, int n);
 void lookupspline(double *t, int *k, double dist, double len, int n);
 double evalspline(double t, const double D[2], const double y[2]);
 
 void DP(double *q1, double *q2, int *n1, int *N1, double *lam1, int *Disp, double *yy) {
-	int i, j, k, l, n, M, N, Eidx, Fidx, Ftmp, Fmin, Num, *Path, *x, *y, cnt;
-	double *q2L, *D, *tmp, *E, Etmp, Emin, t, a, b, lam;
+	int i, j, k, l, n, M, N, Eidx, Fidx, Ftmp, Fmin, Num, *Path, *xy, x, y, cnt;
+	const int scl = 5;
+	const double *q1, *q2;
+	double *q1L, *q2L, *yy, *D1, *D2, *tmp1, *tmp2, *E, Etmp, Emin, t, a, b, lam=0;
 
 	n = *n1;
 	N = *N1;
 	lam = *lam1;
 
-	M = 10*N;
+	M = scl*(N-1)+1;
 
+	q1L = malloc(n*M*sizeof(double));
 	q2L = malloc(n*M*sizeof(double));
 
-	D = malloc(2*N*sizeof(double));
-	tmp = D + N;
-
-	a = 1.0/N;
-	b = 1.0;
+	D1 = malloc(4*N*sizeof(double));
+	tmp1 = D1 + N;
+	D2 = D1 + 2*N;
+	tmp2 = D2 + N;
 
 	for (i = 0; i < n; ++i) {
+
 		for (j = 0; j < N; ++j) {
-			tmp[j] = q2[n*j + i];
+			tmp1[j] = q1[n*j + i];
+			tmp2[j] = q2[n*j + i];
 		}
 
-		spline(D, tmp, N);
+		spline(D1, tmp1, N);
+		spline(D2, tmp2, N);
 
+		// for each point in fine discretization
 		for (j = 0; j < M; ++j) {
-			/* XXX: Extrapolation 1/M < 1/N */
-			lookupspline(&t, &k, (j+1.0)/M - a, b - a, N);
-			q2L[n*j + i] = evalspline(t, D+k, tmp+k);
+			lookupspline(&t, &k, j/(M-1.0), 1, N);
+			q1L[n*j + i] = evalspline(t, D1+k, tmp1+k);
+			q2L[n*j + i] = evalspline(t, D2+k, tmp2+k);
 		}
 	}
 
-	free(D);
+	free(D1);
 
 	E = calloc(N*N, sizeof(double));
 	Path = malloc(2*N*N*sizeof(int));
 
 	for (i = 0; i < N; ++i) {
-		E[N*i + 0] = 100000;
-		E[N*0 + i] = 100000;
+		E[N*i + 0] = 50000000000;
+		E[N*0 + i] = 50000000000;
 		Path[N*(N*0 + i) + 0] = -1;
 		Path[N*(N*0 + 0) + i] = -1;
 		Path[N*(N*1 + i) + 0] = -1;
@@ -94,7 +100,7 @@ void DP(double *q1, double *q2, int *n1, int *N1, double *lam1, int *Disp, doubl
 				l = j - Nbrs[Num][1];
 
 				if (k >= 0 && l >= 0) {
-					Etmp = E[N*l + k] + CostFn2(q1,q2,q2L,k,l,i,j,n,N,M,lam);
+					Etmp = E[N*l + k] + CostFn2(q1L,q2L,k,l,i,j,n,scl,lam);
 					if (Num == 0 || Etmp < Emin) {
 						Emin = Etmp;
 						Eidx = Num;
@@ -111,30 +117,22 @@ void DP(double *q1, double *q2, int *n1, int *N1, double *lam1, int *Disp, doubl
 	free(E);
 	free(q2L);
 
-	/* XXX: x, y assumed to be at most length N */
-	x = malloc(2*N*sizeof(int));
-	y = x + N;
-
-	x[0] = N-1;
-	y[0] = N-1;
+	xy = malloc(2*N*sizeof(int));
+	xy[2*0 + 0] = N-1;
+	xy[2*0 + 1] = N-1;
 
 	cnt = 1;
-	while (x[cnt-1] > 0) {
-		y[cnt] = Path[N*(N*0 + x[cnt-1]) + y[cnt-1]];
-		x[cnt] = Path[N*(N*1 + x[cnt-1]) + y[cnt-1]];
+	while (x = xy[2*(cnt-1) + 0], x > 0) {
+		y = xy[2*(cnt-1) + 1];
+
+		xy[2*cnt + 1] = Path[N*(N*0 + x) + y];
+		xy[2*cnt + 0] = Path[N*(N*1 + x) + y];
 		++cnt;
 	}
 
 	free(Path);
-	for (i = 0, j = cnt-1; i < j; ++i, --j) {
-		k = x[i];
-		x[i] = x[j];
-		x[j] = k;
 
-		k = y[i];
-		y[i] = y[j];
-		y[j] = k;
-	}
+	qsort(xy, cnt, 2*sizeof(int), xycompare);
 
 	for (i = 0; i < N; ++i) {
 
@@ -142,55 +140,59 @@ void DP(double *q1, double *q2, int *n1, int *N1, double *lam1, int *Disp, doubl
 		Fidx = 0;
 
 		for (j = 0; j < cnt; ++j) {
-			Ftmp = (int)fabs(i - x[j]);
+			x = xy[2*j + 0];
+			Ftmp = fabs(i - x);
 			if (j == 0 || Ftmp < Fmin) {
 				Fmin = Ftmp;
 				Fidx = j;
 			}
 		}
 
-		if (x[Fidx] == i) {
-			yy[i] = (y[Fidx]+1);
+		x = xy[2*Fidx + 0];
+		y = xy[2*Fidx + 1];
+
+		if (x == i) {
+			yy[i] = y;
 		}
 		else {
-			if (x[Fidx] > i) {
-				a = x[Fidx] - i;
-				b = i - x[Fidx-1];
-				yy[i] = (a*(y[Fidx-1]+1) + b*(y[Fidx]+1))/(a+b);
+			if (x > i) {
+				a = x - i;
+				b = i - xy[2*(Fidx-1) + 0];
+				yy[i] = (a*xy[2*(Fidx-1) + 1] + b*y)/(a+b);
 			}
 			else {
-				a = i - x[Fidx];
-				b = x[Fidx+1] - i;
-				yy[i] = (a*(y[Fidx+1]+1) + b*(y[Fidx]+1))/(a+b);
+				a = i - x;
+				b = xy[2*(Fidx+1) + 0] - i;
+				yy[i] = (a*xy[2*(Fidx+1) + 1] + b*y)/(a+b);
 			}
 		}
 
-		yy[i] /= N;
+		yy[i] = (yy[i]-yy[0])/(N-1);
 	}
 
-	free(x);
+	free(xy);
 }
 
 int xycompare(const void *x1, const void *x2) {
 	return (*(int *)x1 > *(int *)x2) - (*(int *)x1 < *(int *)x2);
 }
 
-double CostFn2(const double *q1, const double *q2, const double *q2L, int k, int l, int i, int j, int n, int N, int M, double lam) {
+double CostFn2(const double *q1L, const double *q2L, int k, int l, int i, int j, int n, int scl, double lam) {
 	double m = (j-l)/(double)(i-k), sqrtm = sqrt(m), E = 0, y, tmp, ip, fp;
-	int x, idx, d;
+	int x, idx, d, iL=i*scl, kL=k*scl, lL=l*scl;
 
-	for (x = k; x <= i; ++x) {
-		y = (x-k)*m + l + 1;
-		fp = modf(y*M/N, &ip);
-		idx = (int)(ip + (fp >= 0.5)) - 1;
+	for (x = kL; x <= iL; ++x) {
+		y = (x-kL)*m + lL;
+		fp = modf(y, &ip);
+		idx = (int)(ip + (fp >= 0.5));
 
 		for (d = 0; d < n; ++d) {
-            tmp = q1[n*x + d] - q2L[n*idx + d];
+			tmp = q1L[n*x + d] - sqrtm*q2L[n*idx + d];
 			E += tmp*tmp;
 		}
 	}
 
-	return E/N;
+	return E;
 }
 
 void thomas(double *x, const double *a, const double *b, double *c, int n) {
@@ -211,6 +213,8 @@ void thomas(double *x, const double *a, const double *b, double *c, int n) {
 	}
 }
 
+// input:  y is array to interpolate, n is array length
+// output: D will be array of spline data
 void spline(double *D, const double *y, int n) {
 	int i;
 	double *a, *b, *c;
