34ad7abe81f0f6753177fc01550c9cbf58f5ad80
J. Derek Tucker <jdtuck@sandia.gov>
2019-08-06 11:26:23 -0600

    update test example

--- bin/ex_srsf_align.py
+++ bin/ex_srsf_align.py
@@ -3,4 +3,4 @@ import numpy as np
 data = np.load('simu_data.npz')
 time = data['arr_1']
 f = data['arr_0']
-out = fs.srsf_align(f,time,omethod="DP2",showplot=True)
+out = fs.srsf_align(f,time,omethod="RBFGS",showplot=True)
