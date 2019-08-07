92bdf5ea54698aac3220724b20e3800014dca5c4
J. Derek Tucker <jdtuck@sandia.gov>
2019-08-07 14:15:29 -0600

    add to example

--- bin/ex_srsf_align.py
+++ bin/ex_srsf_align.py
@@ -1,6 +1,12 @@
+#%%
 import fdasrsf as fs
 import numpy as np
-data = np.load('simu_data.npz')
+data = np.load('bin/simu_data.npz')
 time = data['arr_1']
 f = data['arr_0']
-out = fs.srsf_align(f,time,omethod="RBFGS",showplot=True)
+out = fs.srsf_align(f,time,omethod="RBFGS",showplot=False)
+jfpca = fs.jointfPCA(out.fn,time,out.qn,out.q0,out.gam, showplot=False)
+samples = fs.joint_gauss_model(out.fn,time,out.qn,out.gam,out.q0)
+
+
+#%%
