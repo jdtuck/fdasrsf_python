2cc2058c1f3dc0747ff7b9568dedb068dc858d8e
J. Derek Tucker <jdtuck@sandia.gov>
2019-08-07 09:20:03 -0600

    working on pca tolerance bound, not complete

--- fdasrsf/tolerance.py
+++ fdasrsf/tolerance.py
@@ -8,6 +8,10 @@ moduleauthor:: Derek Tucker <jdtuck@sandia.gov>
 import numpy as np
 import fdasrsf as fs
 import fdasrsf.utility_functions as uf
+import fdasrsf.fPCA as fpca
+from scipy.stats import chi2
+from numpy.random import randn
+from numpy.linalg import eig
 from fdasrsf.gauss_model import joint_gauss_model
 from fdasrsf.boxplots import ampbox, phbox
 
@@ -32,7 +36,7 @@ def bootTB(f, time, a=0.5, p=.99, B=500, no=5, parallel=True):
     :return ph: phase tolerance bounds
 
     """
-
+    eps = np.finfo(np.double).eps
     (M, N) = f.shape
 
     # Align Data
@@ -59,13 +63,81 @@ def bootTB(f, time, a=0.5, p=.99, B=500, no=5, parallel=True):
         bootupr_ph[:,k] = objp.Q3a
     
     # tolerance bounds
-    boot_amp = np.hstack((bootlwr_amp, bootupr_amp)
-    boot_amp_q = uf.f_to_srsf(boot_amp,time)
+    boot_amp = np.hstack((bootlwr_amp, bootupr_amp))
+    f, g, g2 = uf.gradient_spline(time, boot_amp, False)
+    boot_amp_q = g / np.sqrt(abs(g) + eps)
     boot_ph = np.hstack((bootlwr_ph,bootupr_ph))
     amp = ampbox(boot_amp, out_med.fmean , boot_amp_q, out_med.mqn, time, alpha=a, k_a=.3)
     ph = phbox(boot_ph, time, alpha=a, k_a=.3)
 
     return amp, ph
-    
 
+
+def pcaTB(f, time, a=0.5, p=.99, no=5, parallel=True):
+    """
+    This function computes tolerance bounds for functional data containing
+    phase and amplitude variation using fPCA
+
+    :param f: numpy ndarray of shape (M,N) of N functions with M samples
+    :param time: vector of size M describing the sample points
+    :param a: confidence level of tolerance bound (default = 0.05)
+    :param p: coverage level of tolerance bound (default = 0.99)
+    :param no: number of principal components (default = 5)
+    :param parallel: enable parallel processing (default = T)
+    :type f: np.ndarray
+    :type time: np.ndarray
+
+    :rtype: tuple of boxplot objects
+    :return warp: alignment data from time_warping
+    :return pca: functional pca from jointFPCA
+    :return tol: tolerance factor
+
+    """
+
+    # Align Data
+    warp = fs.srsf_align(f, time, method="median", showplot=False, parallel=parallel)
+
+    # Calculate pca
+    pca = fpca.jointfPCA(warp.fn, time, warp.qn, warp.q0, warp.gam, no=no, showplot=False)
+
+    # Calculate TB
+    tol = mvtol_region(pca.coef, a, p, 100000)
+
+    return warp, pca, tol
+
+
+def mvtol_region(x, alpha, p, B):
+    """
+    Computes tolerance factor for multivariate normal
+    -------------------------------------------------------------------------
+    Krishnamoorthy, K. and Mondal, S. (2006), Improved Tolerance Factors for Multivariate Normal
+    Distributions, Communications in Statistics - Simulation and Computation, 35, 461–478.
     
+    :param x: (M,N) matrix defining N variables of M samples
+    :param alpha: confidence level
+    :param P: coverage level
+    :param B: number of bootstrap samples
+
+    :rtype: double
+    :return tol: tolerance factor
+
+    """
+    n,p = x.shape
+
+    q_squared = chi2.rvs(1, size=(p,B))/n
+    L = 
+
+
+def rwishart(df,p):
+    R = np.zeros((p,p))
+    R = R.flatten()
+    R[::p+1] = np.sqrt(chi2.rvs(np.arange(df,df-p,-1),size=p))
+    if p>1:
+        pseq = np.arange(0,p)
+        tmp = [np.arange(0,x+1) for x in np.arange(0,p-1)]
+        R[np.repeat(p*pseq,pseq)+np.concatenate(tmp).ravel()] = randn(int(p*(p-1)/2))
+
+    R = R.reshape((p,p))
+    R = np.matmul(R.T,R)
+
+    return R
