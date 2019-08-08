5facb2ec5cce16a253b5d37655af6e42e70ce995
J. Derek Tucker <jdtuck@sandia.gov>
2019-08-08 08:27:59 -0600

    add moc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -30,6 +30,8 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphin
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+autodoc_mock_imports = ["curve_functions"]
+
 # The suffix of source filenames.
 source_suffix = '.rst'
 
