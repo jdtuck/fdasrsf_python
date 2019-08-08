3539f63613df2973882defe838e84a3c51f3c72f
J. Derek Tucker <jdtuck@sandia.gov>
2019-08-08 07:46:49 -0600

    reset

--- bin/ex_srsf_align.py
+++ bin/ex_srsf_align.py
@@ -5,5 +5,5 @@ data = np.load('bin/simu_data.npz')
 time = data['arr_1']
 f = data['arr_0']
 out = fs.srsf_align(f,time,omethod="RBFGS",showplot=False)
-fs.bootTB(f, time, a=0.5, p=.99, B=500, no=5, parallel=False)
+
 #%%
