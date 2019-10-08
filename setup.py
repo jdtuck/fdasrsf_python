b94f0475d0f658286827a8d06c2bd1ca474550e9
J. Derek Tucker <jdtuck@sandia.gov>
2019-10-08 11:09:21 -0600

    bump version number to indicate changes to OO

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     cmdclass={'build_ext': build_ext, 'build_docs': build_docs},
 	ext_modules=extensions,
     name='fdasrsf',
-    version='1.5.0',
+    version='2.0.0',
     packages=['fdasrsf'],
     url='http://research.tetonedge.net',
     license='LICENSE.txt',
