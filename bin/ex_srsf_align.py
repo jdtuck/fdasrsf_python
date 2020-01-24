1732c619e9b6c77eb9e9c4a28694cc797a13e23b
J. Derek Tucker <jdtuck@sandia.gov>
2020-01-24 06:26:21 -0700

    update test run

--- bin/ex_srsf_align.py
+++ bin/ex_srsf_align.py
@@ -1,8 +1,8 @@
 #%%
 import fdasrsf as fs
 import numpy as np
-data = np.load('bin/simu_data.npz')
+data = np.load('simu_data.npz')
 time = data['arr_1']
 f = data['arr_0']
 obj = fs.fdawarp(f,time)
-obj.srsf_align()
+obj.srsf_align(parallel=True)
