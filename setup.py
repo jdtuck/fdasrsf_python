5f521faad4efdeb4ee082e41c88cbac3c3383bf3
J. Derek Tukcer <jdtuck@sandia.gov>
2020-09-07 09:27:29 -0600

    update requires for pip

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
     cmdclass={'build_ext': build_ext_subclass, 'build_docs': build_docs},
 	ext_modules=extensions,
     name='fdasrsf',
-    version='2.1.1',
+    version='2.1.2',
     packages=['fdasrsf'],
     url='http://research.tetonedge.net',
     license='LICENSE.txt',
@@ -141,6 +141,7 @@ setup(
         "joblib",
         "patsy",
         "tqdm",
+        "findblas",
     ],
     classifiers=[
         'License :: OSI Approved :: BSD License',
