44e9f2dcc18dd9eb571d4a1019c8da17cda724ac
J. Derek Tukcer <jdtuck@sandia.gov>
2020-09-24 16:48:01 -0600

    more updates, seems to crash python on windows

--- fdasrsf/umap_metric.py
+++ fdasrsf/umap_metric.py
@@ -3,7 +3,7 @@ from numba.core.typing import cffi_utils as cffi_support
 import cffi
 ffi = cffi.FFI()
 import _DP
-from numpy import linspace, interp, zeros, diff, double, sqrt, arange, float64, int64, zeros, frombuffer, ascontiguousarray
+from numpy import linspace, interp, zeros, diff, double, sqrt, arange, float64, int32, zeros, frombuffer, ascontiguousarray, empty
 
 DP = _DP.lib.DP
 cffi_support.register_module(_DP)
@@ -35,20 +35,22 @@ def efda_distance(q1, q2):
         q1 = ascontiguousarray(q1)
         q2 = ascontiguousarray(q2)
 
-        n1_arr = ffi.new("int *")
+        n1_arr=empty(1,dtype=int32)
         n1_arr[0]=n1
-        M_arr= ffi.new("int *")
+        n1_arr_ptr=ffi.from_buffer(n1_arr)
+        M_arr=empty(1,dtype=int32)
         M_arr[0]=M
-        disp_arr= ffi.new("int *")
+        M_arr_ptr=ffi.from_buffer(M_arr)
+        disp_arr=empty(1,dtype=int32)
         disp_arr[0]=disp
-        lam_arr= ffi.new("double *")
+        disp_arr_ptr=ffi.from_buffer(disp_arr)
+        lam_arr=empty(1,dtype=double)
         lam_arr[0]=lam
-        q1_wrap = ffi.cast("double *", ffi.from_buffer(q1))
-        q2_wrap = ffi.cast("double *", ffi.from_buffer(q2))
-        gami = ffi.cast("double *", ffi.from_buffer(gam))
-        DP(q2_wrap,q1_wrap,n1_arr,M_arr,lam_arr,disp_arr,gami)
-
-        gam = frombuffer(ffi.buffer(gami, M*8), dtype=double)
+        lam_arr_ptr=ffi.from_buffer(lam_arr)
+        q1_wrap = ffi.from_buffer(q1)
+        q2_wrap = ffi.from_buffer(q2)
+        gami = ffi.from_buffer(gam)
+        DP(q2_wrap,q1_wrap,n1_arr_ptr,M_arr_ptr,lam_arr_ptr,disp_arr_ptr,gami)
 
         gam_dev = grad(gam, 1 / double(M - 1))
         tmp = interp((time[-1] - time[0]) * gam + time[0], time, q2)
@@ -60,5 +62,3 @@ def efda_distance(q1, q2):
         dist = sqrt(tmp.sum())
        
     return dist
-
-
