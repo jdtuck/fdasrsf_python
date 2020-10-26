fdf7210e008c76b529b8e68d2f37c8ff66e37ff3
J. Derek Tukcer <jdtuck@sandia.gov>
2020-10-26 05:58:10 -0600

    bump version

--- fdasrsf/__init__.py
+++ fdasrsf/__init__.py
@@ -7,7 +7,7 @@ analysis
 """
 __all__ = ["time_warping", "utility_functions", "curve_stats", "geodesic", "curve_functions", "geometry", "pcr_regression", "tolerance", "boxplots", "curve_regression", "regression", "fPCA"]
 
-__version__ = "2.1.11"
+__version__ = "2.1.12"
 
 import sys
 
