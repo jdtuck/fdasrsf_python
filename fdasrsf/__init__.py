84d1815c78a1695afd3da97c49f53b2a1ebdac6b
J. Derek Tucker <jdtuck@sandia.gov>
2021-02-28 18:46:03 -0700

    release bugfix to pypi

--- fdasrsf/__init__.py
+++ fdasrsf/__init__.py
@@ -7,7 +7,7 @@ analysis
 """
 __all__ = ["time_warping", "utility_functions", "curve_stats", "geodesic", "curve_functions", "geometry", "pcr_regression", "tolerance", "boxplots", "curve_regression", "regression", "fPCA"]
 
-__version__ = "2.2.1"
+__version__ = "2.2.3"
 
 import sys
 
--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     cmdclass={'build_ext': build_ext, 'build_docs': build_docs},
 	ext_modules=extensions,
     name='fdasrsf',
-    version='2.2.2',
+    version='2.2.3',
     packages=['fdasrsf'],
     url='http://research.tetonedge.net',
     license='LICENSE.txt',
