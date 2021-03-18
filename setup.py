64bd0dfdf516d340e59e2cbef238b0d09fe101f5
J. Derek Tucker <jdtuck@sandia.gov>
2021-03-18 07:43:44 -0600

    fix issue release

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     cmdclass={'build_ext': build_ext, 'build_docs': build_docs},
 	ext_modules=extensions,
     name='fdasrsf',
-    version='2.2.24',
+    version='2.2.5',
     packages=['fdasrsf'],
     url='http://research.tetonedge.net',
     license='LICENSE.txt',
