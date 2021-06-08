706f868983ef06976aa176bdf9d09d72fb93affc
J. Derek Tucker <jdtuck@sandia.gov>
2021-06-08 13:54:25 -0600

    more updates to bayesian functions

--- fdasrsf/bayesian_functions.py
+++ fdasrsf/bayesian_functions.py
@@ -1,5 +1,8 @@
 import numpy as np
+from numpy.random import multivariate_normal, rand, normal
+from numpy.linalg import solve, lstsq, det
 from scipy.integrate import trapz, cumtrapz
+from scipy.stats import truncnorm, norm
 import fdasrsf.utility_functions as uf
 import cbayesian as bay
 
@@ -34,7 +37,7 @@ def f_vpostlogl_pw(v, q1, q2, var, SSEv):
 def f_f1postlogl_pw(f1,y1,SSEv,K_f1,sigma_curr,sigma1_curr):
     n = y1.shape[0]
     iSig_f1 = K_f1 + np.eye(n) * n/sigma1_curr
-    f1_mean = np.linalg.solve(iSig_f1, y1) * n/sigma1_curr
+    f1_mean = solve(iSig_f1, y1) * n/sigma1_curr
     SSEf1 = (f1-f1_mean) @ iSig_f1 @ (f1 - f1_mean)
     out = -SSEv/(2 * sigma_curr) - SSEf1/2
     return(out)
@@ -43,7 +46,7 @@ def f_f1postlogl_pw(f1,y1,SSEv,K_f1,sigma_curr,sigma1_curr):
 def f_f2postlogl_pw(f2,y2,SSEv,K_f2,sigma_curr,sigma2_curr):
     n = y2.shape[0]
     iSig_f2 = K_f2 + np.eye(n) * n/sigma2_curr
-    f2_mean = np.linalg.solve(iSig_f2, y2) * n/sigma2_curr
+    f2_mean = solve(iSig_f2, y2) * n/sigma2_curr
     SSEf2 = (f2-f2_mean) @ iSig_f2 @ (f2 - f2_mean)
     out = -SSEv/(2 * sigma_curr) - SSEf2/2
     return(out)   
@@ -76,6 +79,152 @@ def f_dlogl_pw(v_coef, v_basis, d_basis, sigma_curr, q1, q2):
 
     return nll, g_coef, SSEv
 
+
+def f_updatef1_pw(f1_curr, q1_curr, y1, q2, v_coef_curr, v_basis, SSE_curr, K_f1, K_f1prop, sigma_curr, sigma1_curr):
+    time = np.linspace(0,1,y1.shape[0])
+    v = uf.f_basistofunction(v_basis["x"], 0, v_coef_curr, v_basis)
+
+    f1_prop = multivariate_normal(f1_curr, K_f1prop)
+    q1_prop = uf.f_to_srsf(f1_prop, time)
+
+    SSE_prop = f_SSEv_pw(v, q1_prop, q2)
+
+    postlog_curr = f_f1postlogl_pw(f1_curr, y1, SSE_curr, K_f1, sigma_curr, sigma1_curr)
+    postlog_prop = f_f1postlogl_pw(f1_prop, y1, SSE_prop, K_f1, sigma_curr, sigma1_curr)
+
+    ratio = np.minimum(1, np.exp(postlog_prop-postlog_curr))
+
+    u = rand()
+    if (u <= ratio):
+        f1_curr = f1_prop
+        q1_curr = q1_prop
+        f1_accept = True
+    else:
+        f1_accept = False
+    
+    return f1_curr, q1_curr, f1_accept
+
+
+def f_updatef2_pw(f2_curr, q2_curr, y2, q1, v_coef_curr, v_basis, SSE_curr, K_f2, K_f2prop, sigma_curr, sigma2_curr):
+    time = np.linspace(0,1,y1.shape[0])
+    v = uf.f_basistofunction(v_basis["x"], 0, v_coef_curr, v_basis)
+
+    f2_prop = multivariate_normal(f2_curr, K_f2prop)
+    q2_prop = uf.f_to_srsf(f2_prop, time)
+
+    SSE_prop = f_SSEv_pw(v, q1, q2_prop)
+
+    postlog_curr = f_f2postlogl_pw(f2_curr, y2, SSE_curr, K_f2, sigma_curr, sigma2_curr)
+    postlog_prop = f_f2postlogl_pw(f2_prop, y2, SSE_prop, K_f2, sigma_curr, sigma2_curr)
+
+    ratio = np.minimum(1, np.exp(postlog_prop-postlog_curr))
+
+    u = rand()
+    if (u <= ratio):
+        f2_curr = f2_prop
+        q2_curr = q2_prop
+        f2_accept = True
+    else:
+        f2_accept = False
+    
+    return f2_curr, q2_curr, f2_accept
+
+
+def f_updatephi_pw(f1_curr, K_f1, s1_curr, L1_curr, L1_propvar, Dmat):
+    a, b = (L1_curr - L1_curr) / L1_propvar, (100000 - L1_curr) / L1_propvar
+    L1_prop = truncnorm.rvs(a, b)
+
+    K_f1_tmp = s1_curr * (uf.exp2corr2(L1_prop,Dmat) +0.1 * np.eye(f1_curr.shape[0]))
+
+    SSEf_curr = (f1_curr @ K_f1 @ f1_curr)/2
+    SSEf_prop = (mrdivide(f1_curr,K_f1_tmp) * f1_curr)/2
+
+    postlog_prop = -np.log(det(K_f1_tmp))/2 - SSEf_prop - np.log(1-norm.cdf(-L1_curr/L1_propvar))
+    postlog_curr = np.log(det(K_f1))/2 - SSEf_curr - np.log(1-norm.cdf(-L1_prop/L1_propvar))
+    ratio = np.minimum(1, np.exp(postlog_prop-postlog_curr))
+
+    u = rand()
+    if (u <= ratio):
+        L1_curr = L1_prop
+        L1_accept = True
+    else:
+        L1_accept = False
+    
+    return L1_curr, L1_accept
+
+
+def f_updatev_pw(v_coef_curr, v_basis, sigma_curr, q1, q2, nll_cur, g_cur, SSE_curr, propose_v_coef, d_basis, cholC, h, L):
+    v_coef_prop = propose_v_coef(v_coef_curr)
+
+    q = v_coef_prop.copy()
+    D = q.shape[0]
+    rth = np.sqrt(h)
+    g_cur = -1*g_cur
+
+    # sample velocity
+    v = normal(size=D)
+    v = cholC.T@v
+
+    # natural gradient
+    halfng = cholC@g_cur
+    ng = cholC@halfng
+
+    # accumulate the power of force
+    pow = rth/2*(g_cur.T@v)
+
+    # calculate current energy
+    E_cur = nll_cur - h/8*(halfng.T@halfng)
+
+    randL = np.ceil(rand()*L)
+    
+    # Alternate full sth for position and velocity
+    for l in range(0, randL):
+        # Make a half step for velocity
+        v = v + rth/2 * ng
+
+        # Make a full step for position
+        rot = (q+1j*v)*np.exp(-1j*rth)
+        q = np.real(rot)
+        v = np.imag(rot)
+
+        # update geometry
+        nll, g, SSEv = f_dlogl_pw(q, v_basis, d_basis, sigma_curr, q1, q2)
+        halfng = cholC@g
+        ng = cholC.T@halfng
+
+        # Make a half step for velocity
+        v = v + rth/2*ng
+
+        # accumulate the power of force
+        if l!=randL:
+            pow = pow + rth*(g.T@v)
+    
+    # accumulate the power of force
+    pow = pow + rth/2 * (g.T@v)
+
+    # evaluate energy at start and end of trajectory
+    E_prop = nll - h/8*(halfng.T@halfng)
+
+    # Accept or reject the state at end of trajectory, returning either
+    # the position at the end of the trajectory or the initial position
+    logRatio = -E_prop + E_cur - pow
+
+    if np.isinf(logRatio) and (np.log(rand()) < np.minimum(0, logRatio)):
+        v_coef_curr = q
+        theta_accept = True
+        SSE_curr = SSEv
+    else:
+        nll = nll_cur
+        g = g_cur
+        theta_accept = False
+
+
+    return v_coef_curr, nll, g, SSE_curr, theta_accept
+
+
+def mrdivide(a, b):
+    c = lstsq(a.T, b.T)
+    return(c)
 
 def f_SSEg_pw(g, q1, q2):
     obs_domain = np.linspace(0,1,g.shape[0])
