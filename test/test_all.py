010b840a2d3cdfa908b8aa75b74f2d065a8e0c82
J. Derek Tucker <jdtuck@sandia.gov>
2022-01-15 15:09:43 -0700

    add more test infrastructure

--- test/test_all.py
+++ test/test_all.py
@@ -12,6 +12,29 @@ class TestFDASRSF(unittest.TestCase):
         gam = fs.optimum_reparam(q1, timet, q1)       
         self.assertAlmostEqual(sum(gam-timet),0)
     
+    def test_warp_f_gamma(self):  
+        M = 101
+        q1 = np.sin(np.linspace(0,2*np.pi,M))
+        timet = np.linspace(0,1,M)
+        gam = fs.optimum_reparam(q1, timet, q1)   
+        q1a = fs.warp_f_gamma(timet, q1, gam)    
+        self.assertAlmostEqual(sum(q1-q1a),0)
+    
+    def test_warp_q_gamma(self):  
+        M = 101
+        q1 = np.sin(np.linspace(0,2*np.pi,M))
+        timet = np.linspace(0,1,M)
+        gam = fs.optimum_reparam(q1, timet, q1)   
+        q1a = fs.warp_q_gamma(timet, q1, gam)    
+        self.assertAlmostEqual(sum(q1-q1a),0)
+    
+    def test_reparm1(self):  
+        M = 101
+        q1 = np.sin(np.linspace(0,2*np.pi,M))
+        timet = np.linspace(0,1,M)
+        gam = fs.optimum_reparam(q1, timet, q1, method="DP")       
+        self.assertAlmostEqual(sum(gam-timet),0)
+    
     def test_rlbgs(self):
         M = 101
         q1 = np.sin(np.linspace(0,2*np.pi,M))
@@ -34,6 +57,29 @@ class TestFDASRSF(unittest.TestCase):
         da, dp = fs.elastic_distance(f1, f1, timet)
         self.assertLessEqual(da, 1e-10)
         self.assertLessEqual(dp, 1e-6)
+    
+    def test_smooth(self):
+        M = 101
+        q1 = np.zeros((M,1))
+        q1[:,0] = np.sin(np.linspace(0,2*np.pi,M)).T
+        q1a = fs.smooth_data(q1,1)
+        q1b = fs.smooth_data(q1,1)
+        self.assertAlmostEqual(sum(q1a.flatten()-q1b.flatten()),0)
+    
+    def test_edistance(self):
+        M = 101
+        q1 = np.sin(np.linspace(0,2*np.pi,M))
+        timet = np.linspace(0,1,M)
+        dy, dx = fs.elastic_distance(q1, q1, timet)
+        self.assertAlmostEqual(sum(dy+dx),0)
+    
+    def test_invgamma(self):
+        M = 101
+        gam = np.linspace(0,1,M)
+        gami = fs.invertGamma(gam)
+        self.assertAlmostEqual(sum(gam-gami),0)
+    
+
   
 if __name__ == '__main__': 
     unittest.main() 
