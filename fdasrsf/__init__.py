e5118d6d14ef5e41652636c3e0d227fbb0437d9d
J. Derek Tucker <jdtuck@sandia.gov>
2022-03-28 16:08:05 -0600

    release to pip

--- fdasrsf/__init__.py
+++ fdasrsf/__init__.py
@@ -9,7 +9,7 @@ __all__ = ["time_warping", "utility_functions", "bayesian_functions", "curve_sta
            "geometry", "pcr_regression", "tolerance", "boxplots", "curve_regression", "regression", "fPCA", 
            "elastic_glm_regression", "curve_pcr_regression"]
 
-__version__ = "2.3.8"
+__version__ = "2.3.9"
 
 import sys
 
