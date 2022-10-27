88971649ee806a0fe1ab4da5d0d2a851a243d47a
Derek Tucker <jdtuck@sandia.gov>
2022-10-27 06:51:08 -0600

    prepare for re;ease

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     cmdclass={'build_ext': build_ext, 'build_docs': build_docs},
 	ext_modules=extensions,
     name='fdasrsf',
-    version='2.3.11',
+    version='2.3.12',
     packages=['fdasrsf'],
     url='http://research.tetonedge.net',
     license='LICENSE.txt',
