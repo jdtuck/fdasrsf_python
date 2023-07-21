dc78c38d09f73bb409c3ee202b7f7fcb048eddfc
J. Derek Tucker <jdtuck@sandia.gov>
2023-07-21 10:37:02 -0600

    code up basic gp as replacement

--- fdasrsf/__init__.py
+++ fdasrsf/__init__.py
@@ -7,7 +7,7 @@ analysis
 """
 __all__ = ["time_warping", "utility_functions", "bayesian_functions", "curve_stats", "geodesic", "curve_functions", 
            "geometry", "pcr_regression", "tolerance", "boxplots", "curve_regression", "regression", "fPCA", 
-           "elastic_glm_regression", "curve_pcr_regression", "kmeans", "image", "image_functions"]
+           "elastic_glm_regression", "curve_pcr_regression", "kmeans", "image", "image_functions", "gp"]
 
 __version__ = "2.4.1"
 
