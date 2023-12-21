0b4cfcaa02d6f6f8331fc5db84a861f5d505dd10
J. Derek Tucker <jdtuck@sandia.gov>
2023-12-21 05:03:39 -0700

    move to mkl on mac

--- bin/cibw_before_build.sh
+++ bin/cibw_before_build.sh
@@ -8,16 +8,18 @@ if [[ $RUNNER_OS == "Linux" || $RUNNER_OS == "macOS" ]] ; then
     basedir=$(python bin/openblas_support.py --use-ilp64)
     if [[ $RUNNER_OS == "macOS" && $PLATFORM == "macosx-arm64" ]]; then
         # /usr/local/lib doesn't exist on cirrus-ci runners
-        sudo mkdir -p /usr/local/lib /usr/local/include /usr/local/lib/cmake/openblas
-        sudo mkdir -p /opt/arm64-builds/lib /opt/arm64-builds/include
-        sudo chown -R $USER /opt/arm64-builds
-        cp -r $basedir/lib/* /opt/arm64-builds/lib/
-        cp $basedir/include/* /opt/arm64-builds/include/
-        sudo cp -r $basedir/lib/* /usr/local/lib/
-        sudo cp $basedir/include/* /usr/local/include/
+        # sudo mkdir -p /usr/local/lib /usr/local/include /usr/local/lib/cmake/openblas
+        # sudo mkdir -p /opt/arm64-builds/lib /opt/arm64-builds/include
+        # sudo chown -R $USER /opt/arm64-builds
+        # cp -r $basedir/lib/* /opt/arm64-builds/lib/
+        # cp $basedir/include/* /opt/arm64-builds/include/
+        # sudo cp -r $basedir/lib/* /usr/local/lib/
+        # sudo cp $basedir/include/* /usr/local/include/
+        pip install mkl-devel
     else
-        cp -r $basedir/lib/* /usr/local/lib/
-        cp $basedir/include/* /usr/local/include/
+        # cp -r $basedir/lib/* /usr/local/lib/
+        # cp $basedir/include/* /usr/local/include/
+        pip install mkl-devel
     fi
 elif [[ $RUNNER_OS == "Windows" ]]; then
     # delvewheel is the equivalent of delocate/auditwheel for windows.
