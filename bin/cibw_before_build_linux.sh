2a284bbc8d0ff1f333af7bafa6fa62cde1b94c0a
J. Derek Tucker <jdtuck@sandia.gov>
2024-05-25 15:47:19 -0600

    started cibuildwheel support

---
+++ bin/cibw_before_build_linux.sh
@@ -0,0 +1,24 @@
+set -xe
+
+
+NIGHTLY_FLAG=""
+
+if [ "$#" -eq 1 ]; then
+    PROJECT_DIR="$1"
+elif [ "$#" -eq 2 ] && [ "$1" = "--nightly" ]; then
+    NIGHTLY_FLAG="--nightly"
+    PROJECT_DIR="$2"
+else
+    echo "Usage: $0 [--nightly] <project_dir>"
+    exit 1
+fi
+
+PLATFORM=$(PYTHONPATH=tools python -c "import openblas_support; print(openblas_support.get_plat())")
+
+printenv
+# Update license
+
+# Install Openblas
+basedir=$(python bin/openblas_support.py $NIGHTLY_FLAG)
+cp -r $basedir/lib/* /usr/local/lib
+cp $basedir/include/* /usr/local/include
