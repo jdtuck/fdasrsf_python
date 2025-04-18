35700aa2be7474cdc434d99d85c668b6d1b1c56e
Carole Hall <93291480+carole-r-hall@users.noreply.github.com>
2025-04-18 10:10:11 -0500

    modified fPCA.fdahpca.project to use self.warpdata.time instead of self.time because self.time = time hadn't been declared in calc_fpca (#69)
    Co-authored-by: Carole Hall <93291480+carolehall523@users.noreply.github.com>

--- fdasrsf/fPCA.py
+++ fdasrsf/fPCA.py
@@ -375,13 +375,13 @@ class fdahpca:
 
         """
 
-        q1 = fs.f_to_srsf(f, self.time)
-        M = self.time.shape[0]
+        q1 = fs.f_to_srsf(f, self.warp_data.time)
+        M = self.warp_data.time.shape[0]
         n = q1.shape[1]
         mq = self.warp_data.mqn
         gam = np.zeros((M, n))
         for ii in range(0, n):
-            gam[:, ii] = fs.optimum_reparam(mq, self.time, q1[:, ii])
+            gam[:, ii] = fs.optimum_reparam(mq, self.warp_data.time, q1[:, ii])
 
         U = self.U
         no = U.shape[1]
