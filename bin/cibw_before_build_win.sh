faf0ec53c424329def486f12d6fb2ecf1c11dfeb
Derek Tucker <jdtuck@sandia.gov>
2026-01-07 08:26:37 -0700

    updates for windows

--- bin/cibw_before_build_win.sh
+++ bin/cibw_before_build_win.sh
@@ -1,2 +1,14 @@
+set -xe
 
-python -m pip install mkl-devel
+PROJECT_DIR="$1"
+
+printenv
+# Update license
+cat $PROJECT_DIR/tools/wheels/LICENSE_win32.txt >> $PROJECT_DIR/LICENSE.txt
+
+# Install OpenBLAS
+python -m pip install scipy-openblas32==0.3.30.0.8
+python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
+
+# delvewheel is the equivalent of delocate/auditwheel for windows.
+python -m pip install delvewheel wheel
