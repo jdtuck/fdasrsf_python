e55ea6554624b0d2e0af033dd7a493ee27591d8e
Derek Tucker <jdtuck@sandia.gov>
2026-01-07 11:56:23 -0700

    fix windows

--- bin/cibw_before_build_win.sh
+++ bin/cibw_before_build_win.sh
@@ -1,1 +1,25 @@
-python -m pip install mkl-devel
+set -xe
+
+PROJECT_DIR="$1"
+
+printenv
+
+# Install OpenBLAS
+python -m pip install -r bin/requirements_openblas.txt
+python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > $PROJECT_DIR/scipy-openblas.pc
+
+lib_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_lib_dir())")
+include_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_include_dir())")
+
+libdir = $(python -c"import sys; print(os.path.join(sys.prefix, "include"))")
+includedir = $(python -c"import sys; print(os.path.join(sys.prefix, "Library", "lib"))")
+
+cp -r $lib_loc/* libdir
+cp $include_loc/*
+
+cp $libdir/libscipy_openblas.dll $libdir/libopenblas.dll
+cp $libdir/libscipy_openblas.lib $libdir/libopenblas.lib
+
+# delvewheel is the equivalent of delocate/auditwheel for windows.
+
+python -m pip install delvewheel wheel mkl-devel
