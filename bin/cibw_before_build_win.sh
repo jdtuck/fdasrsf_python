c74d46c21f93ca28b21fbf1226b07455db24e175
Derek Tucker <jdtuck@sandia.gov>
2026-01-07 12:08:43 -0700

    bugfix

--- bin/cibw_before_build_win.sh
+++ bin/cibw_before_build_win.sh
@@ -11,8 +11,8 @@ python -c "import scipy_openblas32; print(scipy_openblas32.get_pkg_config())" > 
 lib_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_lib_dir())")
 include_loc=$(python -c"import scipy_openblas32; print(scipy_openblas32.get_include_dir())")
 
-libdir = $(python -c"import sys; import os; print(os.path.join(sys.prefix, "include"))")
-includedir = $(python -c"import sys; import os; print(os.path.join(sys.prefix, "Library", "lib"))")
+libdir = $(python -c"import sys; import os; print(os.path.join(sys.prefix, ""include""))")
+includedir = $(python -c"import sys; import os; print(os.path.join(sys.prefix, ""Library"", ""lib""))")
 
 cp -r $lib_loc/* libdir
 cp $include_loc/*
